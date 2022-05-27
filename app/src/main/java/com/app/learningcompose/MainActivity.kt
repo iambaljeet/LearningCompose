ac1a0a0bb34e875167475a9fedd8b28fd9d12aca
Baljeet <ibaljeet@outlook.com>
2022-05-27 21:02:29 +0530

    State Concept.

--- app/src/main/java/com/app/learningcompose/MainActivity.kt
+++ app/src/main/java/com/app/learningcompose/MainActivity.kt
@@ -3,13 +3,17 @@ package com.app.learningcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.app.learningcompose.ui.theme.LearningComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    StateExample1()
                 }
             }
         }
@@ -32,6 +36,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+var size: MutableState<Dp> = mutableStateOf(10.dp)
+var click = 0
+
+@Composable
+fun StateExample1() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Clicked $click times")
+
+        Spacer(modifier = Modifier.height(size.value))
+
+        ElevatedButton(onClick = {
+            size.value += 10.dp
+            click += 1
+        }) {
+            Text(text = "Click Here")
+        }
+    }
 }
 
 @Preview(showBackground = true)
